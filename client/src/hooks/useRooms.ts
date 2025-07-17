0da99d1aeba6a2e39d6d392c300efba326d245c0
Mathbkj <matheusgblasel@hotmail.com>
2025-07-17 01:44:04 -0300

    fix: throw errors to error boundaries

--- client/src/hooks/useRooms.ts
+++ client/src/hooks/useRooms.ts
@@ -5,17 +5,10 @@ export function useRooms(name: string) {
   return useQuery({
     queryKey: ["get-rooms", { name }],
     queryFn: async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:3333/salas?name=${name}`
-        );
-        const data: GetRoomsAPIResponse = await response.json();
-        return data;
-      } catch (err) {
-        if (err instanceof Error) {
-          return "Falha ao carregar salas. Por favor, tente novamente.";
-        }
-      }
+      const response = await fetch(`http://localhost:3333/salas?name=${name}`);
+      const data: GetRoomsAPIResponse = await response.json();
+      return data;
     },
+    throwOnError: true,
   });
 }
