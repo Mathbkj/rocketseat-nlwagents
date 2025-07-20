046ce8bc26ea547477fd6fd53ac2b82f6300fd03
Mathbkj <matheusgblasel@hotmail.com>
2025-07-20 14:08:21 -0300

    change names: server.ts to index.ts

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastifyMultipart } from "@fastify/multipart";
+import { fastify } from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { PORT } from "./envLoader.ts";
+import { getRooms } from "./http/routes/get-rooms.ts";
+import { createRoom } from "./http/routes/create-room.ts";
+import { getRoomQuestions } from "./http/routes/get-room-questions.ts";
+import { createRoomQuestion } from "./http/routes/create-question.ts";
+import { createRoomAudio } from "./http/routes/create-audio.ts";
+import { getRoomAudios } from "./http/routes/get-audios.ts";
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "http://localhost:5173",
+});
+app.register(fastifyMultipart);
+
+app.register(getRooms);
+app.register(getRoomQuestions);
+app.register(getRoomAudios);
+app.register(createRoom);
+app.register(createRoomQuestion);
+app.register(createRoomAudio);
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+async function run() {
+  await app.listen({ port: PORT });
+}
+await run();
