f0f6c76a9f9c3321de45a7e448482e8d9a6dea92
Mathbkj <matheusgblasel@hotmail.com>
2025-07-22 21:12:18 -0300

    patch: remove engines node version in package.json | fix: remove ensureVector.ts(Render PostgreSQL already have)

--- server/src/db/ensureVector.ts
+++
@@ -1,8 +0,0 @@
-import { sql } from "drizzle-orm";
-
-/* IMPORTANTE: Esse script garante que não haja bugs
-ou relacionados à extensão 'vector' na hora de efetuar o deploy da aplicação
-*/
-export function ensureVector() {
-  sql`CREATE EXTENSION IF NOT EXISTS vector`;
-}
